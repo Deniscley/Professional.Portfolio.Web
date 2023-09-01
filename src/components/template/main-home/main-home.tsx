86642e570fd991f7d62197e1272a82b3f809a7f2
Deniscley Marfran <deniscleymaf@gmail.com>
2023-09-01 13:16:33 -0300

    feat: Site and image color adjustments

--- src/components/template/main-home/main-home.tsx
+++ src/components/template/main-home/main-home.tsx
@@ -1,26 +1,56 @@
 import "./main-home.css";
+import { AiOutlineDownload } from "react-icons/ai";
+import { BiLogoLinkedin, BiLogoGithub } from "react-icons/bi";
 
 export default function MainHome() {
   return (
-    <section className="home">
-      <div className="container home__content">
-        <h1>
-          Olá, Eu sou <span>Deniscley Marfran</span>
-        </h1>
-        <div className="home__text-animate">
-          <h3>Full Stack Developer</h3>
-          <p>
-            Este é o site oficial do meu portfólio para estar me apresentando e
-            mostrar todos os meus trabalhos relacionados ao desenvolvimento web.
-          </p>
+    <>
+      <section className="home">
+        <div className="container home__content">
+          <div className="home__left">
+            <h1>
+              Olá, Eu sou <span>Deniscley Marfran</span>
+            </h1>
+            <div className="home__text-animate">
+              <h3>Full Stack Developer</h3>
+              <p>
+                Este é o site oficial do meu portfólio para estar me
+                apresentando e mostrar todos os meus trabalhos relacionados ao
+                desenvolvimento web.
+              </p>
 
-          <div className="home__btn-box">
-            <a href="" className="btn">
-              Download CV
-            </a>
+              <div className="home__btn-box">
+                <a href="" className="btn">
+                  Download CV
+                  <AiOutlineDownload />
+                </a>
+              </div>
+
+              <div className="home__sci">
+                <a href="" target="_blank" rel="noreferrer noopener">
+                  <BiLogoLinkedin />
+                </a>
+                <a href="" target="_blank" rel="noreferrer noopener">
+                  <BiLogoGithub />
+                </a>
+              </div>
+            </div>
+          </div>
+
+          <div className="home__rigth">
+            <div className="home__image">
+              <img
+                src="/images/deniscley_foto2-home.jpg"
+                alt="Main Home Image"
+                width={540}
+                height={600}
+              />
+            </div>
           </div>
         </div>
-      </div>
-    </section>
+        <div className="home-imgHover"></div>
+      </section>
+      <script></script>
+    </>
   );
 }
