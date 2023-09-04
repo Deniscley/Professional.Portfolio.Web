69ca6dd630e32260d16354dbdb8daeb6f2626769
Deniscley Marfran <deniscleymaf@gmail.com>
2023-09-04 12:58:25 -0300

    feat: Education screen creation

--- src/components/template/about/about-template.tsx
+++ src/components/template/about/about-template.tsx
@@ -1,9 +1,9 @@
-import "./about.css";
+import "./about-template.css";
 
 export default function AboutTemplate() {
   return (
-    <section className="about">
-      <h2 className="about__heading">
+    <section className="about" id="about">
+      <h2 className="heading">
         About <span>Me</span>
       </h2>
       <div className="about__img">
--- src/components/template/education/education-template.tsx
+++ src/components/template/education/education-template.tsx
@@ -1,5 +1,57 @@
-import "./education.css";
+import { BiSolidCalendar } from "react-icons/bi";
+import "./education-template.css";
 
 export default function EducationTemplate() {
-  return <>#Educação</>;
+  return (
+    <section className="education" id="education">
+      <h2 className="heading">
+        Minha <span>Jornada</span>
+      </h2>
+      <div className="education__row">
+        <div className="education__column">
+          <h3 className="title">Educação</h3>
+          <div className="education__box">
+            <div className="education__content">
+              <div className="content">
+                <div className="year">
+                  <BiSolidCalendar />
+                  <a> 2012 - 2018</a>
+                </div>
+                <h3>Universidade Federal de Lavras</h3>
+                <p>
+                  Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                  Dolores maxime architecto voluptas facere, officia
+                  voluptatibus veritatis necessitatibus vel, dolorem incidunt
+                  magnam at sit fuga atque distinctio ullam quasi.
+                  Exercitationem, ad.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="education__column">
+          <h3 className="title">Experiência</h3>
+          <div className="education__box">
+            <div className="education__content">
+              <div className="content">
+                <div className="year">
+                  <BiSolidCalendar />
+                  <a> 2019 - 2023</a>
+                </div>
+                <h3>Squadra Tecnologia</h3>
+                <p>
+                  Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                  Dolores maxime architecto voluptas facere, officia
+                  voluptatibus veritatis necessitatibus vel, dolorem incidunt
+                  magnam at sit fuga atque distinctio ullam quasi.
+                  Exercitationem, ad.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
