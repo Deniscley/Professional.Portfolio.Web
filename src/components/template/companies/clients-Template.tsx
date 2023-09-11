9ba8319d869ca33f1bc5d1067d8cf224b003dd9d
Deniscley Marfran <deniscleymaf@gmail.com>
2023-09-11 14:13:54 -0300

    feat: Experience screen creation

--- src/components/template/companies/clients-Template.tsx
+++ src/components/template/companies/clients-Template.tsx
@@ -1,30 +1,50 @@
+"use client";
 import "./clients-template.css";
+import { clients } from "./../../../state/constants/constants";
+
+// import Swiper core and required modules
+import { Pagination } from "swiper/modules";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
 
 export default function ClientsTemplate() {
   return (
     <section className="clients" id="clients">
       <h2 className="heading">
-        Meus <span>Clientes</span>
+        Empresas em que <span>Atuei</span>
       </h2>
 
-      <div className="container companies__container">
-        <div className="clients__subtitle">
-          <p>
-            Tive a oportunidade de trabalhar com um grupo diversificado de
-            empresas. Algumas das empresas notáveis ​com as quais trabalhei
-            incluem.
-          </p>
-        </div>
-
-        <article className="companies">
-          <img
-            src="/images/squadra-logo.png"
-            width={400}
-            height={200}
-            alt="Logo Empresa"
-          />
-        </article>
+      <div className="clients__subtitle">
+        <p>
+          Tive a oportunidade de trabalhar com um grupo diversificado de
+          empresas. Algumas das empresas notáveis ​com as quais trabalhei
+          incluem.
+        </p>
       </div>
+
+      <Swiper
+        className="container clients__container"
+        // install Swiper modules
+        modules={[Pagination]}
+        spaceBetween={40}
+        slidesPerView={1}
+        pagination={{ clickable: true }}
+      >
+        {clients.map(({ id, title, path, site }) => {
+          return (
+            <SwiperSlide className="clients__client" key={id}>
+              <a href={site} target="_blank" rel="noreferrer noopener">
+                <img src={path} width={500} height={300} alt="Logo Empresa" />
+                <h5 className="clients__client-name">{title}</h5>
+              </a>
+            </SwiperSlide>
+          );
+        })}
+      </Swiper>
     </section>
   );
 }
---
+++ src/state/constants/constants.tsx
@@ -0,0 +1,26 @@
+export const clients = [
+  {
+    id: 0,
+    title: "Squadra Digital",
+    path: "/images/squadra-logo.png",
+    site: "https://www.squadra.com.br/",
+  },
+  {
+    id: 1,
+    title: "SEST SENAT",
+    path: "/images/Sest-Senat-logo.jpg",
+    site: "https://www.sestsenat.org.br/home",
+  },
+  {
+    id: 2,
+    title: "FINLÂNDIA - Corretora de Seguros",
+    path: "/images/finlandia-logo.jpg",
+    site: "https://finlandiaseguros.com.br/",
+  },
+  {
+    id: 3,
+    title: "Pottencial - Seguradora",
+    path: "/images/pottencial-logo.png",
+    site: "https://pottencial.com.br/",
+  },
+];
